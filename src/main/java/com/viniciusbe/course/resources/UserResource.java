8bf1b89e8ee933ca2b9017bbd1151fdcb8ece17d
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-05-15 11:57:31 -0300

    feat: create user service and resource methods

--- src/main/java/com/viniciusbe/course/resources/UserResource.java
+++ src/main/java/com/viniciusbe/course/resources/UserResource.java
@@ -1,19 +1,37 @@
 package com.viniciusbe.course.resources;
 
 import com.viniciusbe.course.entities.User;
+import com.viniciusbe.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"Maria","maria@gmail.com","9999999", "12345");
-        return ResponseEntity.ok().body(u);
+    private final UserService service;
 
+    @Autowired
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
